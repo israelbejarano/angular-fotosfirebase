837399ef6e50277a0fe05287502c5d0f71e005a4
israel <israel.bejarano.a@gmail.com>
2019-01-25 17:30:24 +0100

    add routes, services, firebase, and components declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
+// Components
+import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+
+// Routes
+import { APP_ROUTES } from './app.routes';
+
+// Services
+import { CargaImagenesService } from './services/carga-imagenes.service';
+
+
+/**
+ *
+ *
+ * @export
+ * @class AppModule
+ * @author ibejarano
+ */
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FotosComponent,
+    CargaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'my-app-name'), // imports firebase/app needed for everything
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    APP_ROUTES
   ],
-  providers: [],
+  providers: [CargaImagenesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
