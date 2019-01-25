c435170f3ec5bbe011003e427bb0a8f892ee6cfa
israel <israel.bejarano.a@gmail.com>
2019-01-25 17:30:42 +0100

    app components

---
+++ src/app/components/carga/carga.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FileItem } from '../../models/file-item';
+import { CargaImagenesService } from '../../services/carga-imagenes.service';
+
+/**
+ *
+ *
+ * @export
+ * @class CargaComponent
+ * @implements {OnInit}
+ * @author ibejarano
+ */
+@Component({
+  selector: 'app-carga',
+  templateUrl: './carga.component.html',
+  styles: []
+})
+export class CargaComponent implements OnInit {
+
+  archivos: FileItem[] = [];
+
+  constructor(public _cargaImagenes: CargaImagenesService) { }
+
+  ngOnInit() {
+  }
+
+  cargarImagenes() {
+    this._cargaImagenes.cargarImagenesFirebase(this.archivos);
+  }
+
+}
---
+++ src/app/components/fotos/fotos.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ *
+ *
+ * @export
+ * @class FotosComponent
+ * @implements {OnInit}
+ * @author ibejarano
+ */
+@Component({
+  selector: 'app-fotos',
+  templateUrl: './fotos.component.html',
+  styles: []
+})
+
+export class FotosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
