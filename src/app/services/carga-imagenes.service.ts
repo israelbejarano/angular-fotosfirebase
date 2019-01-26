19c9389a048ea2aa11d1b72a594b3894b4fa0485
israel <israel.bejarano.a@gmail.com>
2019-01-26 12:03:40 +0100

    function for upload images to Firebase

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -12,11 +12,35 @@ export class CargaImagenesService {
 
   constructor(private db: AngularFirestore) { }
 
-  private guardarImage(imagen: {nombre: string, url: string}) {
+  private guardarImagen(imagen: {nombre: string, url: string}) {
     this.db.collection(`/${ this.CARPETA_IMAGENES }`).add(imagen);
   }
 
   cargarImagenesFirebase(imagenes: FileItem[]) {
     console.log(imagenes);
+    const storageRef = firebase.storage().ref(); // offical Firebase doc
+    for (const item of imagenes) {
+      item.estaSubiendo = true;
+      if (item.progreso >= 100) {
+        continue;
+      }
+      // offical Firebase doc
+      const uploadTask: firebase.storage.UploadTask =
+          storageRef.child(`${this.CARPETA_IMAGENES}/${item.nombreArchivo}`).put(item.archivo);
+
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot: firebase.storage.UploadTaskSnapshot) => item.progreso =
+                                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+        (error) => console.error('Error al subir', error),
+        () => {
+          console.log('Imagen cargada correctamente');
+          item.url = uploadTask.snapshot.metadata.fullPath;
+          item.estaSubiendo = false;
+          this.guardarImagen({
+            nombre: item.nombreArchivo,
+            url: item.url
+          });
+        });
+    }
   }
 }
